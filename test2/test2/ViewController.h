26d0edb7e470d665b5f9dfa71d0d8c5f07f534c0
Vinod <admin@admins-macbook-pro-2.local>
2016-04-15 10:56:40 +0530

    Testing files checkin

---
+++ test2/test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by Admin on 15/04/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test2/test2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test2
+//
+//  Created by Admin on 15/04/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
